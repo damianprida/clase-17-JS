2117f7fc322f2a0c79a63cd39ae4dc1dec3cd7eb
Laurens.P <damianprida.dp@gmail.com>
2026-09-15 14:46:01 -0300

    clase 18

--- script.js
+++ script.js
@@ -1,99 +1,140 @@
-//DOM document object model
-/*
-Es un objeto que nos permite manipular el html dsde JS.
-getElementById() nos permite obtener un elemento del html a partir de su id.
-usar console.dir para verlo como objeto y ver sus propiedades.
-*/
-/* const title = document.getElementById("title");
-console.dir(title) */
-/* title.innerText = "Hola, mundo!"; MODIFICA EL HTML*/
-/* console.log(title.innerText) /* MUESTRA EL TEXTO DEL HTML */
-/*title.innerText = "Hola, mundo! / MODIFICA EL HTML */
 
-/* let nombre = "pepe";
+const btn_click = document.getElementById('btn')
 
-const caja = document.getElementById("caja")
-caja.innerHTML = `
-    <h2>Hola ${nombre}</h2>
-    <button>Click me</button>
-` */
+
+function saludar() {
+    const div = document.getElementById('contenedor-principal')
+    div.innerHTML = `<h1>Hola que tal!</h1>`
+}
+
 /* 
-dado el producto escribir dentro del elemento con id product-card h2: titulo
-span: precio
+addEventListener es un metodo de los elementos del DOM que permite asociar una funcionalidad a un evento
+Tiene 2 parametros:
+    - (string) Tipo de evento
+    - (funcion) Accion
+*/
+btn_click.addEventListener(
+    'click',
+    saludar
+)
+
+/* 
+Contador
+- El boton de resta decrementara tu contador
+- El boton de suma incrementara tu contador
+- El span mostrara el valor del contador (inicialmente 0)
+
+Recomendacion:
+- Tener una variable en JS con el valor de tu contador y esa variable mostrarla en el span
+
 */
 
-/* const productos = [
-{
-    id : 1,
-    titulo : "tv samsung",
-    precio : 1000,
-    stock : 26,
-},
-{
-    id : 2,
-    titulo : "tv lg",
-    precio : 2000,
-    stock : 56,
-},
-{
-    id : 3,
-    titulo : "tv sony",
-    precio : 3000,
-    stock: 45,
+
+const btnResta = document.getElementById("btn_cont_resta")
+const btnSuma = document.getElementById("btn_cont_suma")
+const spanContador = document.getElementById("contador")
+
+//Esto es un estado
+//Es inmutable
+let contador = 100
+
+function renderContador() {
+    spanContador.textContent = contador
 }
-]
 
-const caja = document.getElementById("caja");
-
-let html_productos = ""
-for(let producto of productos){
-    html_productos = html_productos + `
-    <div>
-        <h2 class="producto_titulo">${producto.titulo}</h2>
-        <div>
-            <div>Precio: <strong>${producto.precio}</strong></div>
-            <div>Stock: <strong>${producto.stock}</strong></div>
-        </div>
-        <button>Comprar</button>
-        <hr/>
-    </div>
-`
+//Esta funcion es la encargada de modificar el estado
+//Cada vez que modifique el valor de mi estado debo volver a renderizar todos los componentes asociados a mi estado
+function setContador(valor) {
+    contador = valor
+    renderContador()
 }
-caja.innerHTML = html_productos */
 
-const mensajes = [
-    {
-        Autor: "pepe",
-        Mensaje: "hola, que tal?",
-        Fecha: "21:30",
-        id: 1
-    },
-    {
-        Autor: "tu",
-        Mensaje: "todo bien y vos?",
-        Fecha: "21:32",
-        id: 2
-    },
-    {
-        Autor: "pepe",
-        Mensaje: "genial",
-        Fecha: "21:35",
-        id: 3
+function sumar() {
+    setContador(contador + 1)
+}
+
+function restar() {
+    setContador(contador - 1)
+}
+
+btnResta.addEventListener(
+    "click",
+    restar
+)
+btnSuma.addEventListener(
+    "click",
+    sumar
+)
+renderContador()
+
+
+/* 
+Selector de opciones
+
+Al clickear una opcion la seleccionare, dependiendo de la opcion que seleccione en el div de opcion seleccionada debe aparecer el numero de la opcion (selecciono opcion 1 y debe decir "Opcion seleccionada: 1")
+
+Ayudita:
+- Todos los elementos estas asociadas a la misma accion
+- La accion es indentificar cual es el option number del boton que 
+*/
+
+/* const button_1 = document.getElementById('btn_1')
+const button_2 = document.getElementById('btn_2')
+console.log(button_1.getAttribute('data-option-number'))
+
+
+function seleccionarOpcion (evento){
+    //El target es el elemento de donde se disparo el evento
+    console.log(evento.target)
+    console.log(evento.target.getAttribute('data-option-number'))
+}
+
+
+button_1.addEventListener('click', seleccionarOpcion)
+button_2.addEventListener('click', seleccionarOpcion) */
+
+const btn1 = document.getElementById("btn_1")
+const btn2 = document.getElementById("btn_2")
+const btn3 = document.getElementById("btn_3")
+const divResultado = document.getElementById("resultado")
+
+let opcion_seleccionada = null
+
+function renderOpcionSeleccionada (){
+    if(opcion_seleccionada){
+        divResultado.innerText = "Opcion seleccionada: " + opcion_seleccionada
     }
-]
-
-const caja = document.getElementById("caja")
-
-let html_mensajes = ""
-
-for(let mensaje of mensajes){
-    html_mensajes = html_mensajes + `
-    <div>
-        <h3>Autor: ${mensaje.Autor}</h3>
-        <p>Mensaje: ${mensaje.Mensaje}</p>
-        <span>Fecha: ${mensaje.Fecha}</span>
-        <hr>
-    </div>
-    `
+    else{
+        divResultado.innerText = 'No has seleccionado ninguna opcion'
+    }
 }
-caja.innerHTML = html_mensajes
+
+function setOpcionSeleccionada (valor){
+    opcion_seleccionada = valor
+    renderOpcionSeleccionada()
+}
+
+function seleccionarOpcion(evento) {
+    const numeroOpcion = evento.target.getAttribute("data-option-number")
+    setOpcionSeleccionada(numeroOpcion)
+}
+
+btn1.addEventListener("click", seleccionarOpcion)
+btn2.addEventListener("click", seleccionarOpcion)
+btn3.addEventListener("click", seleccionarOpcion)
+renderOpcionSeleccionada()
+
+
+const formulario_login = document.getElementById('formulario-login')
+
+function login (evento){
+    evento.preventDefault() //Desactivo el comportamiento por defecto del evento
+    const formulario = evento.target
+    const resultado = {
+        email: formulario.email.value,
+        password: formulario.password.value
+    }
+    console.log(resultado)
+}
+
+formulario_login.addEventListener('submit', login)
